5cb0337e37420ce2e184c1434544683871156ec9
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-03-08 15:00:51 +0300

    added database files

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT, unique_id int NOT NULL, fname VARCHAR(255), lname VARCHAR(255), email VARCHAR(255), password VARCHAR(255), image VARCHAR(255), status VARCHAR(255), PRIMARY KEY (id));
+
+CREATE TABLE chats(chat_id INT NOT NULL AUTO_INCREMENT, incoming_id int NOT NULL, outgoing_id INT NOT NULL, msg VARCHAR(1000), time VARCHAR(255), PRIMARY KEY(chat_id));
